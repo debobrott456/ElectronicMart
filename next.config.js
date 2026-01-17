408e5e95c8d06f27939b9358c30e36f04c919b51
Joy kumar <joyk3075@gmail.com>
2026-01-17 18:58:02 +0600

    added <Image> component

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: {
-    unoptimized: true,
     domains: [
       'via.placeholder.com', 
       'images.unsplash.com',
